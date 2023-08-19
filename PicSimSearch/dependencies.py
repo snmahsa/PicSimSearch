bb1f6750a73bb8439a55622af16adcfc27fbe18f
mahsa <mahsasanaei73@gmail.com>
2023-08-19 17:55:42 +0330

    creat dependences.py

---
+++ PicSimSearch/dependencies.py
@@ -0,0 +1,11 @@
+#Import library
+import cv2
+import tkinter as tk
+from tkinter import filedialog
+import os
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL 
+from io import BytesIO
+# import gradio as gr
