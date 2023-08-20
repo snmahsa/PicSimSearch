fa45933e9a9410c3eccb9ea2e21dbaabbe881e8f
mahsa <mahsasanaei73@gmail.com>
2023-08-20 21:28:54 +0330

    edit

--- PicSimSearch/Io/show.py
+++ PicSimSearch/Io/show.py
@@ -5,12 +5,12 @@ from ..color import to_rgb
 def result(result, query):
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
     
-    # تصویر اول
+    
     axes[0].imshow(cv2.cvtColor(query, cv2.COLOR_BGR2RGB))
     axes[0].axis('off')
     axes[0].set_title('query')
     
-    # تصویر دوم
+  
     result_image = cv2.imread(result)
     axes[1].imshow(cv2.cvtColor(result_image, cv2.COLOR_BGR2RGB))
     axes[1].axis('off')
