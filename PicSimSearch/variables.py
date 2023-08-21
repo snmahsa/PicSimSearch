6c0eba47040f9a026efc7ef5cd156cb5eb2c7982
mahsa <mahsasanaei73@gmail.com>
2023-08-21 19:14:53 +0330

    FEAT_FOLDER_PATH

--- PicSimSearch/variables.py
+++ PicSimSearch/variables.py
@@ -1,2 +1,6 @@
 # FEAT_PATH = None
-FEAT_FOLDER_PATH = 'PicSimSearch/features/'
+from .dependencies import os
+project_dir = os.getcwd()
+folder_name = "features/"
+folder_path = os.path.join(project_dir, folder_name)
+FEAT_FOLDER_PATH = folder_path
